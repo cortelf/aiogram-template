3ec07b9fd01d0eeefd68d171ad84fb29503e993e
cortelf <zdaw07@yahoo.com>
2022-12-27 21:11:05 +0500

    Reformat code

--- app/models/db/id_model.py
+++ app/models/db/id_model.py
@@ -2,5 +2,6 @@ from sqlalchemy import Uuid
 from sqlalchemy.orm import Mapped, mapped_column
 from uuid import UUID, uuid4
 
+
 class IdModel:
     id: Mapped[UUID] = mapped_column(Uuid, primary_key=True, default=uuid4)
